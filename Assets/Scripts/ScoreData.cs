f22303c6145c55e0f151cff054a6d3869e0ee409
Junichi Tanaka <junichi200123@gmail.com>
2021-03-20 18:29:39 +0900

    ScoreDataにmaxComboを追加

--- Assets/Scripts/ScoreData.cs
+++ Assets/Scripts/ScoreData.cs
@@ -5,18 +5,20 @@ using UnityEngine;
 public class ScoreData : MonoBehaviour
 {
     public readonly static ScoreData Instance = new ScoreData();
-    public int score = 0;
-    public int goodCount = 0;
-    public int missCount = 0;
     public string title = "";
     public string imgPath = "";
+    public int score = 0;
+    public int maxCombo = 0;
+    public int goodCount = 0;
+    public int missCount = 0;
 
     public void Reset()
     {
+        this.title = "";
+        this.imgPath = "";
+        this.maxCombo = 0;
         this.score = 0;
         this.goodCount = 0;
         this.missCount = 0;
-        this.title = "";
-        this.imgPath = "";
     }
 }
