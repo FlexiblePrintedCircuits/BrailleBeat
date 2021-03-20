82b9b3e002ba4d1e9ca23ab1b43e6b64fefda2e6
masashi <swim2000616@gmail.com>
2021-03-20 12:22:18 +0900

    実質first commit

---
+++ Assets/Plugins/Scripts/GameManager.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UniRx;
+using UniRx.Triggers;
+
+public class GameManager : MonoBehaviour
+{
+
+}
