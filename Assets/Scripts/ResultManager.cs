823596ce8d6651240554ac5918139071e646d074
s1500729 <dicenull.dev@gmail.com>
2021-03-22 18:45:12 +0900

    Twitterシェアできるようにした

--- Assets/Scripts/ResultManager.cs
+++ Assets/Scripts/ResultManager.cs
@@ -3,58 +3,72 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using UnityEngine.Networking;
 
 public class ResultManager : MonoBehaviour
 {
-	GameObject title;
-	GameObject score;
-	GameObject maxCombo;
-	GameObject goodCount;
-	GameObject missCount;
-	GameObject albumCover;
-	GameObject rank;
+    GameObject title;
+    GameObject score;
+    GameObject maxCombo;
+    GameObject goodCount;
+    GameObject missCount;
+    GameObject albumCover;
+    GameObject rank;
 
-	// Start is called before the first frame update
-	void Start()
-	{
-		// リザルト画面を構成するGameObjectを取得
-		this.title = GameObject.Find("Title");
-		this.score = GameObject.Find("Score");
-		this.maxCombo = GameObject.Find("MaxCombo");
-		this.goodCount = GameObject.Find("GoodCount");
-		this.missCount = GameObject.Find("MissCount");
-		this.albumCover = GameObject.Find("AlbumCover");
-		this.rank = GameObject.Find("Rank");
+    // Start is called before the first frame update
+    void Start()
+    {
+        // リザルト画面を構成するGameObjectを取得
+        this.title = GameObject.Find("Title");
+        this.score = GameObject.Find("Score");
+        this.maxCombo = GameObject.Find("MaxCombo");
+        this.goodCount = GameObject.Find("GoodCount");
+        this.missCount = GameObject.Find("MissCount");
+        this.albumCover = GameObject.Find("AlbumCover");
+        this.rank = GameObject.Find("Rank");
 
-		this.title.GetComponent<Text>().text = ScoreData.Instance.title;
-		this.score.GetComponent<Text>().text = ScoreData.Instance.score.ToString();
-		this.maxCombo.GetComponent<Text>().text = ScoreData.Instance.maxCombo.ToString();
-		this.goodCount.GetComponent<Text>().text = ScoreData.Instance.goodCount.ToString();
-		this.missCount.GetComponent<Text>().text = ScoreData.Instance.missCount.ToString();
-		this.albumCover.GetComponent<Image>().sprite = ScoreData.Instance.img;
-		this.rank.GetComponent<Text>().text = this.getRankText(ScoreData.Instance.score);
-	}
+        this.title.GetComponent<Text>().text = ScoreData.Instance.title;
+        this.score.GetComponent<Text>().text = ScoreData.Instance.score.ToString();
+        this.maxCombo.GetComponent<Text>().text = ScoreData.Instance.maxCombo.ToString();
+        this.goodCount.GetComponent<Text>().text = ScoreData.Instance.goodCount.ToString();
+        this.missCount.GetComponent<Text>().text = ScoreData.Instance.missCount.ToString();
+        this.albumCover.GetComponent<Image>().sprite = ScoreData.Instance.img;
+        this.rank.GetComponent<Text>().text = this.getRankText(ScoreData.Instance.score);
+    }
 
-	public void BackToStartScene()
-	{
-		// 遷移する前にスコアは初期化しておく
-		ScoreData.Instance.Reset();
-		FadeManager.Instance.LoadScene("StartScene", 1f);
-	}
+    public void BackToStartScene()
+    {
+        // 遷移する前にスコアは初期化しておく
+        ScoreData.Instance.Reset();
+        FadeManager.Instance.LoadScene("StartScene", 1f);
+    }
 
-	private string getRankText(int score)
-	{
-		if (score >= 900000)
-			return "S";
-		else if (score >= 800000)
-			return "A";
-		else if (score >= 700000)
-			return "B";
-		else if (score >= 700000)
-			return "C";
-		else if (score >= 600000)
-			return "D";
-		else
-			return "E";
-	}
+    public void Share2Twitter()
+    {
+        string esctext = UnityWebRequest.EscapeURL($@"{ScoreData.Instance.title}をプレイ！
+ランク: {getRankText(ScoreData.Instance.score)}
+スコア: {ScoreData.Instance.score}
+https://junnetworks.github.io/BrailleBeatWebGL/");
+        string esctag = UnityWebRequest.EscapeURL("BrailleBeat");
+        string url = "https://twitter.com/intent/tweet?text=" + esctext + "&hashtags=" + esctag;
+
+        //Twitter投稿画面の起動
+        Application.OpenURL(url);
+    }
+
+    private string getRankText(int score)
+    {
+        if (score >= 900000)
+            return "S";
+        else if (score >= 800000)
+            return "A";
+        else if (score >= 700000)
+            return "B";
+        else if (score >= 700000)
+            return "C";
+        else if (score >= 600000)
+            return "D";
+        else
+            return "E";
+    }
 }
